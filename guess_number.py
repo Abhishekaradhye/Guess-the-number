588c2b12d1c9d59c1ab23d60174ff40edd4ab089
Abhishek Aradhye <114404938+Abhishekaradhye@users.noreply.github.com>
2023-03-21 16:09:36 +0530

    Update guess_number.py

--- guess_number.py
+++ guess_number.py
@@ -18,16 +18,42 @@ def guess(num):
     except ValueError as e:
       print("You did not enter a valid number.")
     
-random_number1 = random.randint(5,10)
-b = 2
-while b !=  random_number1:
-  wish = input('Ready to play ?(y/n) : ')
-  try:
-    if wish.lower() == 'y':
-      num1 = int(input('Enter the limit number here : '))
-      print(guess(num1))
+def play_with_computer():
+  random_number1 = random.randint(5,10)
+  b = 2
+  while b !=  random_number1:
+    wish = input('Ready to play ?(y/n) : ')
+    try:
+      if wish.lower() == 'y':
+        num1 = int(input('Enter the limit number here : '))
+        print(guess(num1))
+      else:
+        print('...Thank you...')
+        break
+    except ValueError as e:
+        print("You did not enter a valid number.")
+
+def let_computer_play(num):
+  low = 1
+  high = num  
+  while True:
+    guess = random.randint(low,high)
+    print("Computer's guess : ",guess)
+    feedback = input(f"\nDid computer guess too low 'l', too high 'h' or correct 'c' ? : ").lower()
+    if feedback =='h':
+      high = guess - 1
+    elif feedback == 'l':
+      low = guess + 1
     else:
-      print('...Thank you...')
+      print(f"Yes, Computer guessed that number {guess} correctly !!!")
       break
-  except ValueError as e:
-      print("You did not enter a valid number.")
+    
+def main():
+  print('How would you like to play ?\n1. Play with computer    or\n2. Let computer play to guess your number.')
+  ch = input("Enter your choice '1' or '2' : ")
+  if ch == '1':
+    print(play_with_computer())
+  elif ch == '2':
+    num2 = int(input('Give computer a limit number here : '))
+    print(let_computer_play(num2))
+main()
